0e689642f033e2fd1ecc4198dbdfe6384d39adb9
Developer-1 <dev@email.com>
2025-06-13 00:21:24 +0530

    added code

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <div>
+      <h1>Frontend (React) App</h1>
+      <p>Welcome to the frontend of this 3-tier app!</p>
+    </div>
+  );
+}
+export default App;
